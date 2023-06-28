608fccdeea7160f2fa28c3d240990df0d911b14c
aleidaarevalo <138034636+aleidaarevalo@users.noreply.github.com>
2023-06-28 11:09:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var box = "puppy"
+box = 5;
+var firstname = "Aleida";
+var lastname = "2";
+var numberOfSiblings = 2;
+var hasPets = true
+
+
+
+
+
+
+
+console.log(firstname);
+console.log(lastname);
+console.log(numberOfSiblings)
+console.log(hasPets)
+
+
+var button  = document.getElementById("hi");
+
+button.addEventListener("click),function(){})
+console.log.apply("click");
